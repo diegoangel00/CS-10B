a24da5eb8f28a68e6d3b3b27816c5991bc1c733f
Rapid Jugernaut <diegoangel00@live.com>
2023-02-13 15:34:17 -0800

    Update classWork7.cpp

--- classWork7.cpp
+++ classWork7.cpp
@@ -5,15 +5,11 @@ class Date {
 public:
 	void print();
 	void read();
-	void set(int month, int day, int year);
+	void set(int inMonth, int inDay, int inYear);
 private:
 	int day;
 	int month;
 	int year;
-
-
-
-
 };
 
 //specification
@@ -23,11 +19,15 @@ void Date::print() {
 }
 
 void Date::read() {
+	char temp;
 	
+	cin>> month >> temp >> day >> temp >> year;
 
 }
-void Date::set(int month, int day, int year) {
-
+void Date::set(int inMonth, int inDay, int inYear) {
+	month = inMonth;
+	day = inDay;
+	year = inYear;
 }
 
 
@@ -36,8 +36,12 @@ int main() {
 	Date date2;
 
 	date1.set(4, 27, 1947);
-
-
-
-
+	cout<<"date 1 should be 4/27/1947: ";
+	date1.print();
+	cout<<endl;
+	cout<<"Enter a date month/day/year ";
+	date2.read();
+	cout<<" the date you entered was: ";
+	date2.print();
+	cout<<endl;
 }
