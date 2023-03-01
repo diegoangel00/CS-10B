59f4d9eb166692370c2c886b80227cc0cdb2d4f2
Rapid Jugernaut <diegoangel00@live.com>
2023-03-01 15:49:40 -0800

    Update classWork9.cpp

--- classWork9.cpp
+++ classWork9.cpp
@@ -7,9 +7,36 @@ using namespace std;
 
 // automatic variables allocated on the stack.
 // dynamic variables allocated in 
+// allocate == new
+// deallocate == delete
 
+
+// Stack vs. Heap
 int main() {
+  int* intptr;
+  intptr = new int;
+  // 2 options calls int constructor
+  // intptr = new int(392);
+  int arraySize;
+  
+  cout<< "Enter the size of the array: ";
+  cin <<arraySize;
+  string* stringArray = new string[arraySize];
 
-
+  
+  *intptr = 392; 
+  //2 ways to write this
+  //*strinArray = "Nicole";
+  strinArray[0] = "Nicole";
+  //*(strinArray+2) = "Ryan";
+  strinArray[2] = "Ryan";
+  
+  cout<<*intptr<<" " << *strinArray << " "
+    << stringArray[0] << " " << *(stringArray+2)
+    << stringArray[2] << endl;
+  
+  //ways tp deallocate *intptr 
+  delete intptr;
+  delete [] stringArray;
 
 }
