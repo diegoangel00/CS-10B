8fd8d0cb73896e08760a3114c5e5e84c50d08376
Rapid Jugernaut <diegoangel00@live.com>
2023-03-01 15:17:18 -0800

    Create classWork9.cpp

---
+++ classWork9.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+// automatic variables == local variables
+// static variables == global variables or "static" variables
+// dynamic variabls
+
+// automatic variables allocated on the stack.
+// dynamic variables allocated in 
+
+int main() {
+
+
+
+}
