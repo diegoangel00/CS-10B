6ceb94e98130e907b1c5881334943bfa1f3c712b
Rapid Jugernaut <diegoangel00@live.com>
2023-03-06 16:42:25 -0800

    Add files via upload

---
+++ classWork10.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <cctype>
+#include <cstring>
+#include <string>
+
+using namespace std;
+
+
+
+/*
+int main() {
+	string x;
+	x = "hello there";
+	cout << x << endl;
+
+	string str;
+	getline(cin, str);
+
+	char str2[127];
+	cin.getline(str2, 128);
+
+
+}*/
