52e82eb9db4be1272f4ac71b5800bfd458f6476a
Rapid Jugernaut <diegoangel00@live.com>
2023-03-15 15:41:25 -0700

    Update FeetInches.h

--- FeetInches.h
+++ FeetInches.h
@@ -5,7 +5,8 @@ public:
 	FeetInches addedTo(const FeetInches& other);
 	FeetInches(int inFeet = 0, int inInches = 0);
 	FeetInches operator+(const FeetInches& other);
-	FeetInches operator++();
+	FeetInches operator++(); //pre increment
+	FeetInches operator++(int); //post increment
 private:
 	int feet;
 	int inches;
