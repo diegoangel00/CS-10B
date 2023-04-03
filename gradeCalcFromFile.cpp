70571d827edfcad0f038fa4637a7a39364187f3a
Rapid Jugernaut <diegoangel00@live.com>
2023-04-03 03:11:26 -0700

    Update gradeCalcFromFile.cpp

--- gradeCalcFromFile.cpp
+++ gradeCalcFromFile.cpp
@@ -18,11 +18,13 @@ int main() {
     /* TODO: Compute student grades and exam averages, then output results to a text file here. */
 	ifstream infile;
 	string filename;
+	ofstream outfile;
 	//this is used if the file name is known
 	//cout << "Enter the name of the file: ";
-	infile.open("StudentInfo.tsv");
-	//cin >> filename;
-	//infile.open(filename);
+	//infile.open("StudentInfo.tsv");
+	outfile.open("report.txt");
+	cin >> filename;
+	infile.open(filename);
 
 	if (!infile) {
 		cout << "couldn't open file." << endl;
@@ -52,11 +54,22 @@ int main() {
 			midterm1Avg = midterm1Avg + Midterm1score;
 			midterm2Avg = midterm2Avg + Midterm2score;
 			gradeAvg = gradeAvg + Finalscore;
-
+         if(90 <= (Midterm1score + Midterm2score + Finalscore)/3)
+            grade = 'A';
+         else if(80 <= (Midterm1score + Midterm2score + Finalscore)/3 && (Midterm1score + Midterm2score + Finalscore)/3 < 90)
+            grade = 'B';
+         else if(70 <= (Midterm1score + Midterm2score + Finalscore)/3 && (Midterm1score + Midterm2score + Finalscore)/3 < 80)
+            grade = 'C';
+         else if(60 <= (Midterm1score + Midterm2score + Finalscore)/3 && (Midterm1score + Midterm2score + Finalscore)/3 < 70)
+            grade = 'D';
+         //(x < 60)
+         else
+            grade = 'F';
 
 			//replaces the line of text that is toChange and change it to the replacement string
 			//this should
-			cout << "It did a thing" << endl;
+			cout << LastName << "   "<< FirstName<<"  "<< Midterm1score<<" "<< Midterm2score<<" "<<Finalscore<<"  "<<grade << endl;
+			outfile << LastName << "\t"<< FirstName<<"\t"<< Midterm1score<<"\t"<< Midterm2score<<"\t"<<Finalscore<< "\t" <<grade << endl;
 			infile >> LastName >> FirstName >> Midterm1score >> Midterm2score >> Finalscore;
 			numPeople++;	//increments the # of people for the average
 
@@ -66,7 +79,8 @@ int main() {
 		midterm2Avg = midterm2Avg / numPeople;
 		gradeAvg = gradeAvg / numPeople;
 		//fixed is decimal point
-		cout <<setprecision(2) << fixed << "Averages: midterm1 " << midterm1Avg << ", midterm2 " << midterm2Avg << ", final " << gradeAvg << endl;
+		cout <<setprecision(2) << fixed << "Averages: midterm1 " << midterm1Avg << ", midterm2 " << midterm2Avg << ", final " << gradeAvg << endl<<endl;
+		outfile <<setprecision(2) << fixed <<endl<< "Averages: midterm1 " << midterm1Avg << ", midterm2 " << midterm2Avg << ", final " << gradeAvg << endl;
 
 	}
 	return 0;
@@ -91,19 +105,12 @@ Bradshaw    Reagan  96  97  88
 Charlton    Caius   73  94  80
 Mayo    Tyrese  88  61  36
 Stern    Brenda  90  86  45
-
 the file report.txt should contain:
-
-
 //export after program is run
-
 Barrett    Edan    70  45  59  F
 Bradshaw    Reagan  96  97  88  A
 Charlton    Caius   73  94  80  B
 Mayo    Tyrese  88  61  36  D
 Stern    Brenda  90  86  45  C
-
 Averages: midterm1 83.40, midterm2 76.60, final 61.60
-
-
 */
