acd9b050d64b9098a1718c31b7d2521998da4f55
Rapid Jugernaut <diegoangel00@live.com>
2023-04-05 17:28:42 -0700

    Add files via upload

---
+++ Fraction2.cpp
@@ -0,0 +1,402 @@
+#include <iostream>
+#include <cassert>
+#include "fraction2.h"
+
+using namespace std;
+
+//assert()
+
+namespace cs_fraction {
+    Fraction::Fraction() {
+
+        numerator = 0;
+        //denominator can't be set 0 
+        denominator = 1;
+
+    }
+
+    Fraction::Fraction(int num) {
+        numerator = num;
+        simplify();
+    }
+
+
+
+    Fraction::Fraction(int num, int den) {
+        //assert was provided but makes sure that the denominator is no 0 when asked as an input
+        //otherwise stores the input numerator and denominator , num and den in respected places
+        assert(den != 0);
+        numerator = num;
+        denominator = den;
+        simplify();
+    }
+
+
+
+
+
+
+    void Fraction::simplify() {
+        //the goal of this function is to simplify a fraction that is passed to it
+
+        //if denominator is negative it multiplies it by -1 and changes the numerator to the opposite sign
+        if (denominator < 0) {
+            denominator = denominator * -1;
+            numerator = numerator * -1;
+        }
+
+
+
+        for (int i = 1; i <= numerator; i++) {
+            //checks if a number can divide numerator and denominator without leaving a remainder
+            if ((numerator % i == 0) && (denominator % i == 0)) {
+
+                //divides both numerator and deonominator by their common factor and assignes it into original Fraction
+                numerator = numerator / i;
+                denominator = denominator / i;
+
+                i = 1; //if there is a number that is divisible then reset and continue going 
+            }
+        }
+    }
+
+
+
+
+
+
+    Fraction Fraction::addedTo(const Fraction& f2) const {
+        // Goal is to add the current Fraction called and the Fraction that was passed to it
+        // finds greatest common denominator and adds numerators
+        // Outputs the simplified result, does not deal with negatives
+        // NEEDS simplify()
+
+        Fraction result;
+        //stores the numerator 
+        result.numerator = numerator * f2.denominator + denominator * f2.numerator;
+        //stores the denominator
+        result.denominator = denominator * f2.denominator;
+        //simplifies the fraction before returning 
+        result.simplify();
+
+        return result;
+    }
+
+
+
+
+
+    Fraction Fraction::subtract(const Fraction& f2) const {
+        // Goal is to subtract the current Fraction called and the Fraction that was passed to it
+        // finds greatest common denominator and subtracts numerators
+        // Outputs the simplified result, does not deal with negatives
+        // NEEDS simplify()
+
+        Fraction result;
+        //stores the numerator 
+        result.numerator = numerator * f2.denominator - denominator * f2.numerator;
+        //stores the denominator
+        result.denominator = denominator * f2.denominator;
+        //simplifies the fraction before returning 
+        result.simplify();
+
+        return result;
+    }
+
+
+
+
+
+    Fraction Fraction::multipliedBy(const Fraction& f2) const {
+        // Goal is to multiply the current Fraction called and the Fraction that was passed to it
+        // Outputs the simplified result, does not deal with negatives
+        // NEEDS simplify()
+
+        Fraction result;
+        //stores the numerator 
+        result.numerator = numerator * f2.numerator;
+        //stores the denominator
+        result.denominator = denominator * f2.denominator;
+        //simplifies the fraction before returning 
+        result.simplify();
+
+        return result;
+    }
+
+
+
+
+
+    Fraction Fraction::dividedBy(const Fraction& f2) const {
+        // Goal is to divide the current Fraction called and the Fraction that was passed to it
+        // Outputs the simplified result 
+        // NEEDS simplify()
+
+        Fraction result;
+        //stores the numerator 
+        result.numerator = numerator * f2.denominator;
+        //stores the denominator
+        result.denominator = denominator * f2.numerator;
+        //simplifies the fraction before returning 
+        result.simplify();
+
+        return result;
+    }
+
+
+
+
+    bool Fraction::isEqualTo(const Fraction& f2) {
+        // Goal is to check if the current Fraction called and the Fraction that was passed to it are the same
+        // Outputs the simplified result 
+        // NEEDS simplify()
+        //checks if fractions are the same without simplification
+        if (numerator * f2.denominator == denominator * f2.numerator) {
+            return true;
+
+        }
+        return false;
+    }
+
+    void Fraction::print() const {
+
+        //prints the unsimplified Fraction
+        cout << numerator << "/" << denominator;
+    }
+
+
+
+    Fraction operator+(const Fraction& f1, const Fraction& f2) {
+        // Goal is to add the current Fraction called and the Fraction that was passed to it
+        // finds greatest common denominator and adds numerators
+        // Outputs the simplified result, does not deal with negatives
+        // NEEDS simplify()
+
+        Fraction result;
+        //stores the numerator 
+        result.numerator = f1.numerator * f2.denominator + f1.denominator * f2.numerator;
+        //stores the denominator
+        result.denominator = f1.denominator * f2.denominator;
+        //simplifies the fraction before returning 
+        result.simplify();
+
+        return result;
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+    Fraction operator-(const Fraction& f1, const Fraction& f2) {
+        // Goal is to subtract the current Fraction called and the Fraction that was passed to it
+    // finds greatest common denominator and subtracts numerators
+    // Outputs the simplified result, does not deal with negatives
+    // NEEDS simplify()
+
+        Fraction result;
+        //stores the numerator 
+        result.numerator = f1.numerator * f2.denominator - f1.denominator * f2.numerator;
+        //stores the denominator
+        result.denominator = f1.denominator * f2.denominator;
+        //simplifies the fraction before returning 
+        result.simplify();
+
+        return result;
+
+    }
+    Fraction operator*(const Fraction& f1, const Fraction& f2) {
+        // Goal is to multiply the current Fraction called and the Fraction that was passed to it
+        // Outputs the simplified result, does not deal with negatives
+        // NEEDS simplify()
+
+        Fraction result;
+        //stores the numerator 
+        result.numerator = f1.numerator * f2.numerator;
+        //stores the denominator
+        result.denominator = f1.denominator * f2.denominator;
+        //simplifies the fraction before returning 
+        result.simplify();
+
+        return result;
+
+    }
+    Fraction operator/(const Fraction& f1, const Fraction& f2) {
+        // Goal is to divide the current Fraction called and the Fraction that was passed to it
+        // Outputs the simplified result 
+        // NEEDS simplify()
+
+        Fraction result;
+        //stores the numerator 
+        result.numerator = f1.numerator * f2.denominator;
+        //stores the denominator
+        result.denominator = f1.denominator * f2.numerator;
+        //simplifies the fraction before returning 
+        result.simplify();
+
+        return result;
+
+    }
+
+
+
+
+    bool operator<(const Fraction& f1, const Fraction& f2) {
+        Fraction leftNumerator;
+        Fraction rightNumerator;
+        //stores the numerator 
+        //result.numerator = numerator * f2.denominator + denominator * f2.numerator;
+        leftNumerator = f1.numerator * f2.denominator;
+        rightNumerator = f1.denominator * f2.numerator;
+        //simplifies the fraction before returning 
+
+        return (f1.numerator * f2.denominator < f1.denominator* f2.numerator);
+
+    }
+
+
+    bool operator>(const Fraction& f1, const Fraction& f2) {
+        Fraction leftNumerator;
+        Fraction rightNumerator;
+        //stores the numerator 
+        //result.numerator = numerator * f2.denominator + denominator * f2.numerator;
+        leftNumerator = f1.numerator * f2.denominator;
+        rightNumerator = f1.denominator * f2.numerator;
+        //simplifies the fraction before returning 
+
+        return (f1.numerator * f2.denominator > f1.denominator * f2.numerator);
+
+
+    }
+
+    bool operator<=(const Fraction& f1, const Fraction& f2) {
+        Fraction leftNumerator;
+        Fraction rightNumerator;
+        //stores the numerator 
+        //result.numerator = numerator * f2.denominator + denominator * f2.numerator;
+        leftNumerator = f1.numerator * f2.denominator;
+        rightNumerator = f1.denominator * f2.numerator;
+        //simplifies the fraction before returning 
+
+        return (f1.numerator * f2.denominator <= f1.denominator * f2.numerator);
+
+    }
+
+    bool operator>=(const Fraction& f1, const Fraction& f2) {
+        Fraction leftNumerator;
+        Fraction rightNumerator;
+        //stores the numerator 
+        //result.numerator = numerator * f2.denominator + denominator * f2.numerator;
+
+        //
+        leftNumerator = f1.numerator * f2.denominator;
+        rightNumerator = f1.denominator * f2.numerator;
+        //simplifies the fraction before returning 
+
+        return (f1.numerator * f2.denominator >= f1.denominator * f2.numerator);
+
+    }
+
+    bool operator !=(const Fraction& f1, const Fraction& f2) {
+
+        Fraction leftNumerator;
+        Fraction rightNumerator;
+        //stores the numerator 
+        //result.numerator = numerator * f2.denominator + denominator * f2.numerator;
+
+        //
+        leftNumerator = f1.numerator * f2.denominator;
+        rightNumerator = f1.denominator * f2.numerator;
+        //simplifies the fraction before returning 
+
+        return !(f1.numerator * f2.denominator == f1.denominator * f2.numerator);
+
+
+    }
+    bool operator==(const Fraction& f1, const Fraction& f2) {
+        Fraction leftNumerator;
+        Fraction rightNumerator;
+        //stores the numerator 
+        //result.numerator = numerator * f2.denominator + denominator * f2.numerator;
+
+        //
+        leftNumerator = f1.numerator * f2.denominator;
+        rightNumerator = f1.denominator * f2.numerator;
+        //simplifies the fraction before returning 
+
+        return (f1.numerator * f2.denominator == f1.denominator * f2.numerator);
+
+
+
+    }
+
+    Fraction Fraction::operator+=(const Fraction& f2)
+    {
+        *this = *this + f2;
+
+        // or:
+        //feet += right.feet;
+        //inches += right.inches;
+        //simplify();
+
+        return *this;
+    }
+    Fraction Fraction::operator-=(const Fraction& f2)
+    {
+        *this = *this - f2;
+
+        // or:
+        //feet += right.feet;
+        //inches += right.inches;
+        //simplify();
+
+        return *this;
+    }
+    Fraction Fraction::operator*=(const Fraction& f2)
+    {
+        *this = *this * f2;
+
+        // or:
+        //feet += right.feet;
+        //inches += right.inches;
+        //simplify();
+
+        return *this;
+    }
+    Fraction Fraction::operator/=(const Fraction& f2)
+    {
+        *this = *this / f2;
+
+        // or:
+        //feet += right.feet;
+        //inches += right.inches;
+        //simplify();
+
+        return *this;
+    }
+
+
+    ostream& operator<<(ostream& out, Fraction& right){
+
+    }
+
+}
+
+//output
+/*
+The result starts off at 0/1
+The product of 9/8 and 2/3 is 3/4
+The quotient of 9/8 and 2/3 is 27/16
+The sum of 9/8 and 2/3 is 43/24
+The difference of 9/8 and 2/3 is 11/24
+The two Fractions are not equal.
+The product of 3/2 and 2/3 is 1/1
+*/
+
---
+++ Fraction2.h
@@ -0,0 +1,135 @@
+#ifndef FRACTION2_H
+#define FRACTION2_H
+
+/*
+        //constructor
+        Fraction();
+        pre: [no parameters]
+        post: [assigns o inro num, and 1 into den (no division by zero]
+
+
+
+        Fraction(int num, int den);
+        pre: [any int value, an assertion is made so that den will never be 0]
+        post: [assigns num into numerator, and den into deninator]
+
+
+
+        //These fraction classes aren't changing their input so they can be const
+        Fraction addedTo(const Fraction& f2) const;
+        pre: [f2 must be a fraction variable being passed by name]
+        post: [output is fraction variable of the simplest fraction, left in fractional form
+                even if simplification to whole number is available]
+
+        returns a simplified fraction variable
+
+
+
+
+
+        Fraction subtract(const Fraction& f2) const;
+        pre: [f2 must be a fraction variable being passed by name]
+        post: [output is fraction variable of the simplest fraction, left in fractional form
+                even if simplification to whole number is available]
+
+        returns a simplified fraction variable
+
+
+
+
+
+        Fraction multipliedBy(const Fraction& f2) const;
+        pre: [f2 must be a fraction variable being passed by name]
+        post: [output is fraction variable of the simplest fraction, left in fractional form
+                even if simplification to whole number is available]
+
+        returns a simplified fraction variable
+
+
+
+
+
+
+
+        Fraction dividedBy(const Fraction& f2) const;
+        pre: [f2 must be a fraction variable being passed by name]
+        post: [output is fraction variable of the simplest fraction, left in fractional form
+                even if simplification to whole number is available]
+
+        returns a simplified fraction variable
+
+
+
+
+
+
+        bool isEqualTo(const Fraction& f2);
+        pre: [f2 must be a fraction variable being passed by name]
+        post: [will return either flase or true
+            depending on the evaluation of the passed and called fraction variables]
+
+        returns a boolean variable
+
+
+        void print() const
+        pre: [pre condition here]
+        post: [post condition here]
+        no return, prints to the screen
+
+*/
+
+
+namespace cs_fraction {
+    class Fraction
+    {
+    public:
+        //constructor
+        Fraction();
+        Fraction(int num);
+        Fraction(int num, int den);
+
+        //These fraction classes aren't changing their input so they can be const
+        Fraction addedTo(const Fraction& f2) const;
+        Fraction subtract(const Fraction& f2) const;
+        Fraction multipliedBy(const Fraction& f2) const;
+        Fraction dividedBy(const Fraction& f2) const;
+
+        bool isEqualTo(const Fraction& f2);
+
+        void print() const;
+
+        friend Fraction operator+(const Fraction& f1, const Fraction& f2);
+        friend Fraction operator-(const Fraction& f1, const Fraction& f2);
+        friend Fraction operator*(const Fraction& f1, const Fraction& f2);
+        friend Fraction operator/(const Fraction& f1, const Fraction& f2);
+
+
+        //taken from Feetinches
+        friend bool operator<(const Fraction& f1, const Fraction& f2);
+        friend bool operator>(const Fraction& f1, const Fraction& f2);
+        friend bool operator<=(const Fraction& f1, const Fraction& f2);
+        friend bool operator>=(const Fraction& f1, const Fraction& f2);
+        friend bool operator!=(const Fraction& f1, const Fraction& f2);
+        friend bool operator==(const Fraction& f1, const Fraction& f2);
+
+
+        Fraction operator+=(const Fraction& f2);
+        Fraction operator-=(const Fraction& f2);
+        Fraction operator*=(const Fraction& f2);
+        Fraction operator/=(const Fraction& f2);
+
+
+        friend std::ostream& operator<<(std::ostream& out, Fraction& right);
+
+    private:
+        /*[Called to simplify fractions variables, is called in the constructer
+        * and also after certain public functions operate on the fractions
+        * that are called and passed.
+        * This is to ensure that all fractions can be simplified even after
+        * they have been opearated on]*/
+        void simplify();
+        int numerator;
+        int denominator;
+    };
+}
+#endif
