974dc3a731e2b88c8dc25ccc41bd13ce785a5b26
Rapid Jugernaut <diegoangel00@live.com>
2023-04-10 17:22:56 -0700

    Update DynamicClasses.cpp

--- DynamicClasses.cpp
+++ DynamicClasses.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
-    #include "InventoryItem.h"
-    using namespace std;
+#include "InventoryItem.h"
+using namespace std;
 
 int main()
 {
@@ -38,7 +38,22 @@ int main()
     cout << "after item1 = item1, " << endl;
     cout << "item1 is " << item1 << endl;
     cout << "item1 is " << item1 << endl << endl;
+
+    //Overloading Square Brackets
+    cout << item1[1] << item1[2] << item1[3] << endl;
+    item1[1] = 'c';
+    item1[2] = 'i';
+    item1[3] = 's';
+    cout << item1[1] << item1[2] << item1[3] << endl;
+
+    InventoryItem item4("chair");
+    cout << item4[1];
+    item4[1] = 'z';
+    cout << item4[1]<<endl;
+
+
 }
+
 
 void f(InventoryItem item1) {
     item1.setInfo("pizza", 67);
