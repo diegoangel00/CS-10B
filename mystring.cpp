edc6e6d7539c64e25f2cebd45faa5820f3690b53
Rapid Jugernaut <diegoangel00@live.com>
2023-04-24 17:19:33 -0700

    Add files via upload

--- mystring.cpp
+++ mystring.cpp
@@ -8,8 +8,8 @@
 
 using namespace std;
 
-namespace cs_mystring{
-    MyString::MyString(){
+namespace cs_mystring {
+    MyString::MyString() {
         cstring = new char[1];
         strcpy(cstring, "");
     }
@@ -30,7 +30,7 @@ namespace cs_mystring{
 
     MyString MyString::operator=(const MyString& right) {
         if (this != &right) {
-            
+
             delete[] cstring;
             cstring = new char[strlen(right.cstring) + 1];
             strcpy(cstring, right.cstring);
@@ -41,51 +41,81 @@ namespace cs_mystring{
     }
 
 
-    int MyString::length() const{
+    int MyString::length() const {
         return strlen(cstring);
     }
 
 
+    MyString operator+(const MyString& f1, const MyString& f2) {
+
+        MyString result = new char[MyString::MAX_INPUT_SIZE + 1];
+        
+        strcpy(result.cstring, f1.cstring);
+        strcat(result.cstring, f2.cstring);
+
+        return result;
+
+
+    }
+
 
     bool operator<(const MyString& f1, const MyString& f2) {
 
-        return (f1.cstring <  f2.cstring);
+        //return (f1.cstring < f2.cstring);
+        return (strcmp(f1.cstring , f2.cstring) < 0);
 
     }
 
 
     bool operator>(const MyString& f1, const MyString& f2) {
 
-
-        return (f1.cstring > f2.cstring);
+        //return (f1.cstring > f2.cstring);
+        return (strcmp(f1.cstring, f2.cstring) > 0);
     }
 
     bool operator<=(const MyString& f1, const MyString& f2) {
 
-        return (f1.cstring <= f2.cstring);
-
+        //return (f1.cstring <= f2.cstring);
+        return (strcmp(f1.cstring, f2.cstring) <= 0);
     }
 
     bool operator>=(const MyString& f1, const MyString& f2) {
 
-        return (f1.cstring >= f2.cstring);
-
+        //return (f1.cstring >= f2.cstring);
+        return (strcmp(f1.cstring, f2.cstring) >= 0);
     }
 
     bool operator !=(const MyString& f1, const MyString& f2) {
-
-        return (f1.cstring != f2.cstring);
+        return (strcmp(f1.cstring, f2.cstring) != 0);
+        //return (f1.cstring != f2.cstring);
 
     }
     bool operator==(const MyString& f1, const MyString& f2) {
 
-        return (f1.cstring == f2.cstring);
+        //return (f1.cstring == f2.cstring);
+        return (strcmp(f1.cstring, f2.cstring) == 0);
 
     }
 
+    MyString MyString::operator+=(const MyString& f2)
+    {
+        *this = *this + f2;
 
 
+        return *this;
+    }
 
+
+    void MyString::read(istream& in, char target) {
+        
+        char temp[MyString::MAX_INPUT_SIZE + 1];
+        delete[] cstring;
+
+        cstring = new char[MAX_INPUT_SIZE + 1];
+        in.getline(temp, MyString::MAX_INPUT_SIZE, target);
+        strcpy(cstring,temp );
+
+    }
 
     ostream& operator<<(ostream& out, const MyString& source)
     {
@@ -93,7 +123,22 @@ namespace cs_mystring{
         return out;
     }
 
+    istream& operator>>(istream& in, MyString& target) {
+        while (isspace(in.peek())) {
+            in.ignore();
+        }
 
+        char temp[MyString::MAX_INPUT_SIZE + 1];
+        //in.getline(temp, MyString::MAX_INPUT_SIZE, ':');
+
+        delete[] target.cstring;
+        target.cstring = new char[strlen(temp) + 1];
+        strcpy(target.cstring, temp);
+        in >> target.cstring;
+
+        return in;
+
+    }
 
 
     char MyString::operator[](int index) const
@@ -109,6 +154,5 @@ namespace cs_mystring{
     }
 
 }
-
 
 
--- mystring.h
+++ mystring.h
@@ -1,41 +1,50 @@
+#pragma once
 #ifndef MYSTRING_H
 #define MYSTRING_H
 //No documentation required for 11P
 #include <iostream>
 
 
-namespace cs_mystring{
-    class MyString{
-        public:
-            static const int MAX_INPUT_SIZE = 127;
-            //constructor
-            MyString();
-            MyString(const char* inDesc);
-            MyString(const MyString& right);
-            ~MyString();
+namespace cs_mystring {
+    class MyString {
+    public:
+        static const int MAX_INPUT_SIZE = 127;
+        //constructor
+        MyString();
+        MyString(const char* inDesc);
+        MyString(const MyString& right);
+        ~MyString();
 
-            //assginment operator
-            MyString operator=(const MyString& right);
-            int length() const;
+        void read(std::istream& in, char target);
 
-            //output operator
-            friend std::ostream& operator<<(std::ostream& out, const MyString& source);
-          
+        //assginment operator
+        MyString operator=(const MyString& right);
+        int length() const;
+
+        //stream operator
+        friend std::ostream& operator<<(std::ostream& out, const MyString& source);
+        friend std::istream& operator>>(std::istream& in, MyString& target);
 
 
-            //boolean operators
-            friend bool operator<(const MyString& f1, const MyString& f2);
-            friend bool operator>(const MyString& f1, const MyString& f2);
-            friend bool operator<=(const MyString& f1, const MyString& f2);
-            friend bool operator>=(const MyString& f1, const MyString& f2);
-            friend bool operator!=(const MyString& f1, const MyString& f2);
-            friend bool operator==(const MyString& f1, const MyString& f2);
+        MyString operator+=(const MyString& f2);
+        
+        friend MyString operator+(const MyString& f1, const MyString& f2);
 
-            char operator[](int index) const;
-            char& operator[](int index);
+        
 
-        private:
-            char* cstring;
+        //boolean operators
+        friend bool operator<(const MyString& f1, const MyString& f2);
+        friend bool operator>(const MyString& f1, const MyString& f2);
+        friend bool operator<=(const MyString& f1, const MyString& f2);
+        friend bool operator>=(const MyString& f1, const MyString& f2);
+        friend bool operator!=(const MyString& f1, const MyString& f2);
+        friend bool operator==(const MyString& f1, const MyString& f2);
+
+        char operator[](int index) const;
+        char& operator[](int index);
+
+    private:
+        char* cstring;
     };
 }
 #endif
