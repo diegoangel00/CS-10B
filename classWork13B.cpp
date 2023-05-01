49deeedc6d79534930df0f725b824872ebf243e3
Rapid Jugernaut <diegoangel00@live.com>
2023-05-01 15:39:52 -0700

    Update classWork13B.cpp

--- classWork13B.cpp
+++ classWork13B.cpp
@@ -20,12 +20,22 @@ int main() {
     print(v);
 }
 
-
-
-
 void print(MyVector<int>& printMe) {
-    for (int i = 0; i < printMe.size(); i++) {
-        cout << printMe.at(i) << " ";
+    //tries to run this code, when it fails catch is used
+    try {
+        for (int i = 0; i < printMe.size(); i++) {
+            cout << printMe.at(i) << " ";
+            if (i == 20) {
+                cout << printMe.at(50) << endl;
+            }
+        }
+        cout << endl;
+        cout <<"now print printMe.at(50) " << printMe.at(50) << endl;
+    }/*
+    catch (string e) {
+        cout << "ERROR: MyVector index out of range." << endl;
+    }*/
+    catch (MyVector<int>::outOfRangeError e) {
+        cout << "ERROR: MyVector index out of range " << endl;
     }
-    cout << endl;
 }
