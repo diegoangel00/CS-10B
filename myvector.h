d910408eb0cab0ca012320cebd80e26cf85814cb
Rapid Jugernaut <diegoangel00@live.com>
2023-05-01 15:39:35 -0700

    Update myvector.h

--- myvector.h
+++ myvector.h
@@ -1,23 +1,28 @@
-#pragma once
+//#pragma once
+#include <cassert>
 #include <cstdlib>
 
 template <class T>
 class MyVector {
-	public:
-		//std::size_t is a provided function
-		typedef std::size_t size_type;
-		// originally was 
-		// typedef int value_type;
-		typedef T value_type;
-		MyVector(size_type inSize = 0, const value_type & inValue = value_type());
-		size_type size() const;
-		size_type capacity() const;
-		value_type at(size_type i) const;
-		void push_back(const value_type& insertMe);
-	private:
-		value_type* items;
-		size_type mSize;
-		size_type mCapacity;
+public:
+	//to throw errors 
+	class outOfRangeError {};
+
+	//std::size_t is a provided function
+	typedef std::size_t size_type;
+	// originally was 
+	// typedef int value_type;
+	typedef T value_type;
+	MyVector(size_type inSize = 0, const value_type& inValue = value_type());
+	size_type size() const;
+	size_type capacity() const;
+	value_type at(size_type i) const;
+	void push_back(const value_type& insertMe);
+	//void print(MyVector<int>& printMe);
+private:
+	value_type* items;
+	size_type mSize;
+	size_type mCapacity;
 };
 
 #include "myvector.cpp"
